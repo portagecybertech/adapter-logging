93eef7bc3fdb68a4a980a668dc8eea6e377f7af9
Ash <ashley.rosecrans@portagecybertech.com>
2025-10-22 08:58:38 -0400

    Adding tests

--- logger_test.go
+++ logger_test.go
@@ -57,7 +57,29 @@ func TestNamed(t *testing.T) {
 
 func TestNew(t *testing.T) {
 	log := New()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	LOG_ENV := os.Getenv("LOG_ENV")
+	LOG_LEVEL := os.Getenv("LOG_LEVEL")
+
 	if log == nil {
 		t.Errorf("New failed")
+	}
+
+	if len(LOG_LEVEL) > 0 {
+		if strings.ToUpper(LOG_LEVEL) != log.Level().CapitalString() {
+			t.Errorf("Log level mismatch")
+		}
+	} else {
+		if LOG_ENV == "dev" && log.Level().CapitalString() != "INFO" {
+			t.Errorf("Log level default setting for dev is incorrect")
+		}
+		if LOG_ENV != "dev" && log.Level().CapitalString() != "ERROR" {
+			t.Errorf("Log level default setting for non-dev environment is incorrect")
+		}
 	}
 }
